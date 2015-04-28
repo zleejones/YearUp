c049217c3bd039ebdd3446b065b891b01528fdc3
zleejones <zleejones@atl.yearup.org>
2015-04-28 10:07:08 -0400

    adding intro.java

---
+++ Project1/src/Intro.java
@@ -0,0 +1,12 @@
+
+public class Intro {
+
+	public static void main(String[] args) 
+	{
+		 System.out.println("Hello! I'm Zakir. Welcome to the Java class");
+		 // At first I had the two forward slashes in front of the System.out and it would not run
+		 // I also had the s in System lowercase and it would not run
+
+	}
+
+}
